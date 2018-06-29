02885f0695e3102eb4c36094c28e0045e591f6d6
Joshua Thomas Wise <joshua.thomaswise@vice.com>
2018-06-29 17:06:11 -0400

    setup example cratle

--- cases/3.stability-and-robustness/3.1.abnormal-input/uri-normalization.js
+++ cases/3.stability-and-robustness/3.1.abnormal-input/uri-normalization.js
@@ -1,1 +1,12 @@
 'use strict';
+const example = require('../../../util/example');
+
+async function koa(app, port) {
+
+}
+
+async function vapr(app, port) {
+
+}
+
+example.koa(koa).then(() => example.vapr(vapr));
---
+++ util/example.js
@@ -0,0 +1,38 @@
+'use strict';
+const { promisify } = require('util');
+const http = require('http');
+const clc = require('cli-color');
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const Vapr = require('vapr');
+
+exports.koa = async (fn) => {
+  const app = new Koa;
+  const router = new KoaRouter;
+  const server = http.createServer(app.callback());
+  app.use(router.routes());
+  app.use(router.allowedMethods());
+  return promisify(server.listen.bind(server, 0))()
+    .then(() => fn(router, server.address().port))
+    .then(success('Koa'), failure('Koa'))
+    .then(() => server.close());
+};
+
+exports.vapr = async (fn) => {
+  const app = Vapr();
+  const server = http.createServer(app);
+  return promisify(server.listen.bind(server, 0))()
+    .then(() => fn(app, server.address().port))
+    .then(success('Vapr'), failure('Vapr'))
+    .then(() => server.close());
+};
+
+const success = (name) => (value) => {
+  console.log(clc.green(`${name}:`));
+  console.log(String(value));
+};
+
+const failure = (name) => (reason) => {
+  console.log(clc.red(`${name} (ERROR):`));
+  console.log(String(reason));
+};
