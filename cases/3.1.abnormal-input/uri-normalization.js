5b630e1007fa4c097484d5ddaa029ac9769e6295
Joshua Wise <joshuathomaswise@gmail.com>
2018-07-03 19:48:15 -0400

    moved files

--- cases/3.1.abnormal-input/uri-normalization.js
+++ cases/3.1.abnormal-input/uri-normalization.js
@@ -1,6 +1,6 @@
 'use strict';
-const example = require('../../../util/example');
-const send = require('../../../util/send');
+const example = require('../../util/example');
+const send = require('../../util/send');
 
 const URI = `/${encodeURIComponent('terms&privacy')}`;
 
@@ -8,14 +8,14 @@ async function koa(app, port) {
 	app.get('/terms&privacy', (ctx) => { ctx.body = 'this is the correct page'; });
 	app.get('/:page', (ctx) => { ctx.body = 'this is the WRONG page'; });
 	const response = await send(port, `GET ${URI} HTTP/1.1\n\n`);
-	return response.body;
+	return `GET ${URI}\n${response.body}`;
 }
 
 async function vapr(app, port) {
 	app.get('/terms&privacy', () => [['this is the correct page']]);
 	app.get('/:page', () => [['this is the WRONG page']]);
 	const response = await send(port, `GET ${URI} HTTP/1.1\n\n`);
-	return response.body;
+	return `GET ${URI}\n${response.body}`;
 }
 
 example.koa(koa).then(() => example.vapr(vapr));
